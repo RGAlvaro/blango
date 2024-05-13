cbf90c9ed313bd29eb75e68d9bafeae615826511
Álvaro Ruiz <ruizgarciaalvaro92@gmail.com>
2024-05-13 11:31:17 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -91,12 +91,17 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
         "debug_toolbar",
         'blango_auth',
+        "allauth",
+        "allauth.account",
+        "allauth.socialaccount",
+        "allauth.socialaccount.providers.google",
     ]
 
     MIDDLEWARE = [
@@ -194,6 +199,14 @@ class Dev(Configuration):
     # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+    #AUTH
+
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
 class Prod(Dev):
   BASE_DIR = Path(__file__).resolve().parent.parent
